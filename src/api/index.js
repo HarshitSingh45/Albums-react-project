d0402f81c092c02305a9317382553d209f5ba485
HarshitSingh45 <harshitsing45@gmail.com>
2022-05-20 17:13:10 +0530

    added minor changes

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,10 @@
+// common function to make api requests
 const customFetch = async(url, {body, ...customConfig}) => {
+    // defining header
     const headers = {
         'Content-type': 'application/json; charset=UTF-8',
     }
+    // setting config
     const config = {
         ...customConfig,
         headers: {
@@ -9,19 +12,22 @@ const customFetch = async(url, {body, ...customConfig}) => {
             ...customConfig.headers
         },
     };
-
+    // adding body to the config
     config.body = body
 
     try{
+        // sending request to the api
         const response = await fetch(url, config);
         const data = await response.json();
 
+        // if we receive data from the api then we pass otherwise we throw error
         if(data){
             return {
                 data,
                 success: true 
             }
         }
+        // throwing error if req is unsuccessful
         throw new Error("Error occured");
     }catch(error){
         console.error('ERROR');
@@ -30,11 +36,13 @@ const customFetch = async(url, {body, ...customConfig}) => {
         };
     }
 };
+// to fetch all albums from api
 export const getAlbums = () => {
     return customFetch('https://jsonplaceholder.typicode.com/albums', {
         method: 'GET'
     });
 }
+// to add album into albums list
 export const addAlbum = (userid, title) => {
     return customFetch('https://jsonplaceholder.typicode.com/albums', {
         method: 'POST',
@@ -44,6 +52,7 @@ export const addAlbum = (userid, title) => {
             })
 })
 }
+// to update any album data
 export const updateAlbum = (id, userid, title) => {
     return customFetch(`https://jsonplaceholder.typicode.com/albums/${id}`, {
         method: 'PUT',
@@ -54,6 +63,7 @@ export const updateAlbum = (id, userid, title) => {
             })
 })
 }
+// to delete any album from the list
 export const deleteAlbum = (id) => {
     return customFetch(`https://jsonplaceholder.typicode.com/albums/${id}`, {
         method: 'DELETE'
